7a5f9fc1f075f9cefdc2a67f5093a4234f4dcd5f
Shubhrose Singh <shubhroses@gmail.com>
2022-08-16 19:30:15 -0700

    Working jump game

--- medium_questions/55_Jump_Game.py
+++ medium_questions/55_Jump_Game.py
@@ -36,6 +36,12 @@ class Solution:
         if goal == 0:
             return True
         return False
+
+        #Working solutionmax_reach, n = 0, len(nums)
+        for i, x in enumerate(nums):
+            if max_reach < i: return False
+            if max_reach >= n - 1: return True
+            max_reach = max(max_reach, i + x)
         
         
         
