92e8ee81fd2e94d6e2d9e6ccd6990d48bd17c4c5
Shubhrose Singh <shubhroses@gmail.com>
2022-08-22 11:03:11 -0700

    Did it using no extra space

--- medium_questions/238_Product_Of_Array_Except_Self.py
+++ medium_questions/238_Product_Of_Array_Except_Self.py
@@ -9,6 +9,8 @@ class Solution:
                       
         r =    [24, 12, 4, 1]
         """
+
+        #Using n space
         n = len(nums)
         
         l = [1]*n
@@ -22,3 +24,16 @@ class Solution:
         res = [l[i] * r[i] for i in range(n)]
         
         return res
+
+        # Constant space
+        n = len(nums)
+        l = [1]*n
+        
+        for i in range(1, n):
+            l[i] = l[i-1] * nums[i-1]
+        c = nums[-1]
+        for i in range(n-2, -1, -1):
+            l[i] = l[i] * c
+            c *= nums[i]
+            
+        return l
