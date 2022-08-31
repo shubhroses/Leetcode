20e782a0c80d07d7c04d56780c44236a1b0e7804
Shubhrose Singh <shubhroses@gmail.com>
2022-08-30 18:37:15 -0700

    Did sliding window maximum on my own

---
+++ hard_question/239_Sliding_Window_Maximum.py
@@ -0,0 +1,28 @@
+import heapq
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        """
+        max heap with number and index 
+        
+        1. create heap from 0-k
+        2. As we step through nums one my one
+        3. Add new (element, index) to heap
+        4. If top of heap now out of index, pop
+        5. Append to res the top of heap
+        """
+        #Double ended queue
+        
+        n1 = [(-1*nums[i], i) for i in range(k)]
+        heapq.heapify(n1)
+        
+        res = [-1*n1[0][0]]
+        
+        for i in range(k, len(nums)):
+            heapq.heappush(n1, (-1*nums[i], i))
+            topInd = n1[0][1]
+            while topInd > i or topInd < i-k+1:
+                heapq.heappop(n1)
+                topInd = n1[0][1]
+            res.append(-1*n1[0][0])
+        return res
