293672455c73f14f243b8d21c5687294ea835583
Shubhrose Singh <shubhroses@gmail.com>
2022-09-01 11:08:00 -0700

    Did neetcode solution

--- medium_questions/148_Sort_List.py
+++ medium_questions/148_Sort_List.py
@@ -46,3 +46,66 @@ class Solution:
                 
         return res
             
+        #Neet code solution
+        def getMid(self, head):
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+    
+    def merge(self, list1, list2):
+        tail = dummy = ListNode()
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+            
+        if list1:
+            tail.next = list1
+        if list2:
+            tail.next = list2
+        return dummy.next
+        
+    
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        #Base case
+        if not head or not head.next:
+            return head
+        
+        #Split the list into two halves
+        left = head
+        right = self.getMid(head)
+        tmp = right.next
+        right.next = None
+        right = tmp
+        
+        #Keep splitting until one node left
+        left = self.sortList(left)
+        right = self.sortList(right)
+        
+        #Combined split up elements
+        return self.merge(left, right)
+        
+        """
+        Merge Sort:
+        
+        4 -> 2 -> 1 -> 3 -> 
+        
+        4 -> 2 ->
+        1 -> 3 -> 
+        
+        4 ->
+        2 ->
+        1 ->
+        3 ->
+        
+        2 -> 4 -> 
+        1 -> 3 -> 
+        
+        1 -> 2 -> 3 -> 4 -> 
+        """
