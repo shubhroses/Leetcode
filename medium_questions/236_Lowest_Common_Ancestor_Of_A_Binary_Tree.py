8afa9b4b4bafd384b66fc88e29b1e141545a40f6
Shubhrose Singh <shubhroses@gmail.com>
2022-09-05 11:15:04 -0700

    Did lowest common ancestor of a binary tree

---
+++ medium_questions/236_Lowest_Common_Ancestor_Of_A_Binary_Tree.py
@@ -0,0 +1,15 @@
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root == p or root == q:
+            return root
+        
+        left = right = None
+        if root.left:
+            left = self.lowestCommonAncestor(root.left, p, q)
+        if root.right:
+            right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root
+        else:
+            return left or right
