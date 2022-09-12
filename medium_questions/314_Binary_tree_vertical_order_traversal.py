245245a12abdb2da6e3d6dbdde63d5966a622e8c
Shubhrose Singh <shubhroses@gmail.com>
2022-09-12 10:43:25 -0700

    Did it without sorting

--- medium_questions/314_Binary_tree_vertical_order_traversal.py
+++ medium_questions/314_Binary_tree_vertical_order_traversal.py
@@ -7,7 +7,7 @@
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         """
-        Have the confidence to move forward from creating plan to writing code. 
+        Have the confidence to move forward from creating plan to writing code.
         a:0
         b:-1
         c:1
@@ -43,3 +43,20 @@ class Solution:
                 queue.append((node.left, column-1))
                 queue.append((node.right, column +1))
         return [columnTable[x] for x in sorted(columnTable.keys())]
+
+        queue = deque([(root, 0)])
+        adj_list = collections.defaultdict(list)
+        mn, mx = float("inf"), float("-inf")
+        while queue:
+            top, dist = queue.popleft()
+            if top:
+                queue.append((top.left, dist-1))
+                queue.append((top.right, dist+1))
+                adj_list[dist].append(top.val)
+                mn, mx = min(mn, dist), max(mx, dist)
+        res = []
+        if mn == float("inf") or mx == float("-inf"):
+            return []
+        for i in range(int(mn), int(mx+1)):
+            res.append(adj_list[i])
+        return res
