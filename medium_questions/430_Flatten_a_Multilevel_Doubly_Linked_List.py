9c8d8680d33992808010dbf7f4fb59ee88d0c369
Shubhrose Singh <shubhroses@gmail.com>
2022-09-19 19:25:14 -0700

    Did flatten a multilevel double linked list:

---
+++ medium_questions/430_Flatten_a_Multilevel_Doubly_Linked_List.py
@@ -0,0 +1,40 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+"""
+
+class Solution:
+    def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        """
+        r
+        1
+        2
+        3
+              
+        saved = []
+        """
+        if not head:
+            return
+        pseudoHead = Node(0, None, head, None)
+        prev = pseudoHead
+        
+        stack = [head]
+        
+        while stack:
+            curr = stack.pop()
+            prev.next = curr
+            curr.prev = prev
+            
+            if curr.next:
+                stack.append(curr.next)
+            if curr.child:
+                stack.append(curr.child)
+                curr.child = None
+            prev = curr
+        pseudoHead.next.prev = None
+        return pseudoHead.next
