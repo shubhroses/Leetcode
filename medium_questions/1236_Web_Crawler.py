4089169cd207f7c0d2eb021c092524a82f9e3b2e
Shubhrose Singh <shubhroses@gmail.com>
2022-09-25 12:07:03 -0700

    BFS with queue solution

--- medium_questions/1236_Web_Crawler.py
+++ medium_questions/1236_Web_Crawler.py
@@ -20,6 +20,25 @@ class Solution:
         return removedHttp[:indSlash]
         
     def crawl(self, startUrl: str, htmlParser: 'HtmlParser') -> List[str]:
+
+        visited = {startUrl}
+        domain = startUrl.split("http://")[1].split("/")[0]
+        ans = [startUrl]
+        queue = collections.deque([startUrl])
+        while queue:
+            for _ in range(len(queue)):
+                url = queue.popleft()
+                check = htmlParser.getUrls(url)
+                for new_url in check:
+                    if new_url in visited:
+                        continue
+                    if new_url.split("http://")[1].split("/")[0] != domain:
+                        continue
+                    ans.append(new_url)
+                    visited.add(new_url)
+                    queue.append(new_url)        
+        return ans
+
         visited = set()
         result = [startUrl]
         def helper(su):
