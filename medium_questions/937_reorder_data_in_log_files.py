da7ebda546e3afc563982463951dea55dee27b4d
Shubhrose Singh <shubhroses@gmail.com>
2022-11-20 17:35:47 -0800

    resave

--- medium_questions/937_reorder_data_in_log_files.py
+++ medium_questions/937_reorder_data_in_log_files.py
@@ -27,3 +27,52 @@ class Solution:
         let_list.sort(key = lambda x:x.split()[0]) # Sort my start
         let_list.sort(key = lambda x:x.split()[1:]) # Sort my suffix
         return let_list + dig_list
+
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        """
+        All letter logs before digit logs
+        Sort letter logs first by the contents and then by the identifier
+        
+        Step 1:
+        digitLogs = ["dig1 8 1 5 1","dig2 3 6",]
+        letterLogs = ["let1 art can","let2 own kit dig","let3 art zero"]
+        
+        Step 2: Sort letterLogs by identifiers
+        digitLogs = ["dig1 8 1 5 1","dig2 3 6"]
+        letterLogs = ["let1 art can","let2 own kit dig", "let3 art zero"]
+        
+        step 3: Sort letterLogs by contents
+        digitLogs = ["dig1 8 1 5 1","dig2 3 6"]
+        letterLogs = ["let1 art can","let3 art zero", "let2 own kit dig"]
+        
+        Step 4:
+        return letterLogs + digitLogs
+
+        """
+        
+        # Step 1
+        digitLogs = []
+        letterLogs = []
+        
+        for log in logs:
+            elements = log.split()
+            if elements[1].isdigit():
+                digitLogs.append(elements)
+            else:
+                letterLogs.append(elements)
+        
+        # Step 2
+        letterLogs.sort()
+        
+        # Step 3
+        letterLogs.sort(key = lambda x: x[1:])
+        
+        # Step 4
+        res = []
+        for log in letterLogs:
+            res.append(" ".join(log))
+        
+        for log in digitLogs:
+            res.append(" ".join(log))
+        return res
