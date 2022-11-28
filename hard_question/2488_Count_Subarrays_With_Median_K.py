49482f11550b4db86ff709d7afb0eb348539bcdc
Shubhrose Singh <shubhroses@gmail.com>
2022-11-27 22:12:26 -0800

    resave

--- hard_question/2488_Count_Subarrays_With_Median_K.py
+++ hard_question/2488_Count_Subarrays_With_Median_K.py
@@ -1,35 +1,35 @@
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
-        """
-        Need to find the median of every sub array 
+        # Find the index of k in nums
+        index = nums.index(k)
         
+        # l (larger) is count of element>k
+        # s (smaller) is count of element<k
+        res = l1 = s1 = 0
+        before = defaultdict(int)
         
-        Brute force
-            Look at every subarray 
-            Determine median
-            If median == k: increment count 
+        # run from index-1 to 0 (before index)
+        for i in reversed(range(index)):
+            # Increase l or s, and increase the differece in 'before'
+            if nums[i] > k: l1 += 1
+            else: s1 += 1
+            before[(l1-s1)] += 1
             
-        [3,2,1,4,5]  
-         i
-           j
-           
-       [1, 2, 3, 4]
-    
-       len(subarray) = 4 // 2 = 2
-       
-        How to remove sorting 
-        
-        How to find median of unsorted array 
-        
-        [1 2 3 4 5]
-       
-        """
-        res = 0
-        for i in range(len(nums)):
-            for j in range(i, len(nums)):
-                subArray = nums[i:j+1]
-                """
-                Use quick select to find the len(subArray)//2 largest element in subArray 
+            # If the number of larger element and smaller element are the same,
+            # we find a valid subarray which is nums[i:index+1], so increase res.
+            if l1-s1==1 or l1-s1==0: res += 1
+
+        l2 = s2 = 0
+        for i in range(index+1,len(nums)):
+            if nums[i] > k: l2 += 1
+            else: s2 += 1
+            
+            # we need the number of larger elements and smaller elements to be the same in a subarray,
+            #    l1 + l2 == s1 + s2 or l1 + l2 + 1 == s1 + s2
+            # => l1 - s1 == s2 - l2 or l1 - s1 == s2 - l2 + 1
+            # so we need to check if s2-l2 or s2-l2+1 exist in before
+            res += before[s2-l2] + before[s2-l2+1]
+            
+            if l2-s2==1 or l2-s2==0: res += 1
                 
-                """
-        return res
+        return res+1
