cc7473d29e3703fb6cc10a9452d935c58bbb15c3
Shubhrose Singh <shubhroses@gmail.com>
2022-12-16 19:03:13 -0800

    resave:

--- easy_questions/104_Max_Depth_Of_BST.py
+++ easy_questions/104_Max_Depth_Of_BST.py
@@ -9,3 +9,18 @@ class Solution:
         if not root:
             return 0
         return max(1+self.maxDepth(root.left), 1+self.maxDepth(root.right))
+
+        # Iterative
+        stack = []
+        if root is not None:
+            stack.append((1, root))
+        
+        depth = 0
+        while stack != []:
+            current_depth, root = stack.pop()
+            if root is not None:
+                depth = max(depth, current_depth)
+                stack.append((current_depth + 1, root.left))
+                stack.append((current_depth + 1, root.right))
+        
+        return depth
