f45d9738de5668b7223b00bce3767620f0e0ec52
Shubhrose Singh <shubhroses@gmail.com>
2022-12-23 21:38:14 -0800

    resave

--- hard_question/297_Serialize_and_Deserialize_Binary_Tree.py
+++ hard_question/297_Serialize_and_Deserialize_Binary_Tree.py
@@ -35,6 +35,37 @@ class Codec:
         node.right = self.deserialize(self.data[self.data.find(',')+1:])
         return node
 
+    def serialize(self, root):
+        
+        def rserialize(root, string):
+            if root is None:
+                string += "None,"
+            else:
+                string += str(root.val) + ","
+                string = rserialize(root.left, string)
+                string = rserialize(root.right, string)
+            return string
+        return rserialize(root, "")
+        
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        def rdeserialize(l):
+            if l[0] == "None":
+                l.pop(0)
+                return None
+            root = TreeNode(l[0])
+            l.pop(0)
+            root.left = rdeserialize(l)
+            root.right = rdeserialize(l)
+            return root
+        data_list = data.split(",")
+        root = rdeserialize(data_list)
+        return root
         
         
 
