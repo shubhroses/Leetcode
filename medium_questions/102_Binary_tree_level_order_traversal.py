79bd240f664e03e7470ef34a6cca123a7a3a9b8f
Shubhrose Singh <shubhroses@gmail.com>
2022-12-23 21:30:02 -0800

    resave

--- medium_questions/102_Binary_tree_level_order_traversal.py
+++ medium_questions/102_Binary_tree_level_order_traversal.py
@@ -27,3 +27,35 @@ class Solution:
                     q.append(top.right)
             res.append(cur)
         return res
+
+        # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+        res = []
+
+        if not root:
+            return []
+
+        """
+        Level order traversal uses bfs, so have to use a queue
+
+        """
+        q = collections.deque()
+        q.append(root)
+
+        while q:
+            cur = []
+            for _ in range(len(q)):
+                top = q.popleft()
+                cur.append(top.val)
+
+                if top.left:
+                    q.append(top.left)
+                if top.right:
+                    q.append(top.right)
+            res.append(cur)
+        return res
