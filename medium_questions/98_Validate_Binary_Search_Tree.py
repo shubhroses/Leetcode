cf84c37f4bfb5fa9a2b6866a8753451417dd4c9e
Shubhrose Singh <shubhroses@gmail.com>
2022-12-31 14:48:04 -0800

    resave

--- medium_questions/98_Validate_Binary_Search_Tree.py
+++ medium_questions/98_Validate_Binary_Search_Tree.py
@@ -14,3 +14,31 @@ class Solution:
                 return False
             return helper(node.right, node.val, high) and helper(node.left, low, node.val)
         return helper(root)
+
+        # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        """
+        Need a helper function that return true if within bounds?
+        lowerbound: -inf
+        upperbound: inf
+
+        Update lower and upper bounds when calling function again, based on current node value  
+        """
+        def helper(node = None, lower = -inf, upper = inf):
+            if not node:
+                return True
+            if node.val <= lower or node.val >= upper:
+                return False
+            
+            left = helper(node.left, lower, node.val)
+            right = helper(node.right, node.val, upper)
+            return left and right
+            
+
+        return helper(root)
