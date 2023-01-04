e6d3c85df10f466aa5b321e4ca19d774912649a6
Shubhrose Singh <shubhroses@gmail.com>
2023-01-04 00:05:14 -0800

    resave

--- medium_questions/235_Lowest_common_ancestor_of_a_BST.py
+++ medium_questions/235_Lowest_common_ancestor_of_a_BST.py
@@ -20,3 +20,29 @@ class Solution:
                 cur = cur.left
             else:
                 return cur
+
+        # Recursive
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        # Value of current node or parent node.
+        parent_val = root.val
+
+        # Value of p
+        p_val = p.val
+
+        # Value of q
+        q_val = q.val
+
+        # If both p and q are greater than parent
+        if p_val > parent_val and q_val > parent_val:    
+            return self.lowestCommonAncestor(root.right, p, q)
+        # If both p and q are lesser than parent
+        elif p_val < parent_val and q_val < parent_val:    
+            return self.lowestCommonAncestor(root.left, p, q)
+        # We have found the split point, i.e. the LCA node.
+        else:
+            return root
