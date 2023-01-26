e226e992862d29c6398c4843bbc5b44ecfdf8956
Shubhrose Singh <shubhroses@gmail.com>
2023-01-25 22:02:20 -0800

    resave

--- medium_questions/1248_Count_number_of_nice_subarrays.py
+++ medium_questions/1248_Count_number_of_nice_subarrays.py
@@ -13,3 +13,21 @@ class Solution:
             if k == 0:
                 ans += index_of_left_most_odd_in_win - low_bound
         return ans
+
+        dic = {0:1}
+        count = 0
+        res = 0
+
+        for ind, num in enumerate(nums):
+
+            print(f"num: {num}, dic: {dic}, res:{res}")
+            if num % 2 == 1:
+                count += 1
+
+            if count - k in dic:
+                res += dic[count - k]
+            
+            dic[count] = dic.get(count, 0) + 1
+
+        print(res)
+        return res
