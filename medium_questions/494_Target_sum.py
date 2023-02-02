d1658c5ed83c7c836361b3133647c35eeb9ca0dc
Shubhrose Singh <shubhroses@gmail.com>
2023-02-02 14:44:19 -0800

    494

---
+++ medium_questions/494_Target_sum.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        """
+        At each index we can add or subtract that value to total
+
+        base case:
+            If current total equals target return 1
+
+        add current value to current total and re call function
+        subtract current value to current total and re call function
+
+        Save index & value
+        """
+        dp = {}
+        def helper(ind, curTotal):
+            if (ind, curTotal) in dp:
+                return dp[(ind, curTotal)]
+            if curTotal == target and ind == len(nums):
+                return 1
+            if ind == len(nums):
+                return 0
+            dp[(ind, curTotal)] = helper(ind+1, curTotal + nums[ind]) + helper(ind+1, curTotal - nums[ind])
+            return dp[(ind, curTotal)]
+
+        return helper(0, 0)
