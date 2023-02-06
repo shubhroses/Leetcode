b61c122c5872452db5a9404b512b9fecc97fe599
Shubhrose Singh <shubhroses@gmail.com>
2023-02-05 17:57:30 -0800

    209

--- medium_questions/209_Minimum_Size_Subarray_Sum.py
+++ medium_questions/209_Minimum_Size_Subarray_Sum.py
@@ -10,3 +10,33 @@ class Solution:
                 l += 1
             
         return 0 if res == float("inf") else res
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        """
+         0,1,2,3,4,5,6
+        [2,3,1,2,4,3]
+                   l
+                     r
+
+        curSum = 3
+        target = 7
+        res = 3
+        """
+        l = 0
+        r = 0
+        curSum = 0
+        res = float("inf")
+        while r <= len(nums) and l <= len(nums):
+            if curSum >= target: # move left
+                res = min(res, r-l)
+                curSum -= nums[l]
+                l += 1
+            else: # move right
+                if r == len(nums):
+                    break
+                curSum += nums[r]
+                r += 1
+        if res == float("inf"):
+            return 0
+        return res
