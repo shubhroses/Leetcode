0b1a8ed3d7d89719b4f9120c18af397738c86c71
Shubhrose Singh <shubhroses@gmail.com>
2023-02-05 18:50:35 -0800

    1004

--- medium_questions/1004_Max_Consecutive_ones_iii.py
+++ medium_questions/1004_Max_Consecutive_ones_iii.py
@@ -29,3 +29,24 @@ class Solution:
             if zeroCount <= k:
                 res = max(res, r-l+1)
         return res
+
+
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        """
+        find substring in nums satisfying condition that
+
+        len(substring) - count(ones) <= k
+        """
+        l = 0
+        res = 0
+        onesCount = 0
+        for r, n in enumerate(nums):
+            if n == 1:
+                onesCount += 1
+            while r-l+1 - onesCount > k:
+                if nums[l] == 1:
+                    onesCount -=1
+                l+=1
+            res = max(res, r-l+1)
+        return res
