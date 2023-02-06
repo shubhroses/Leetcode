7098c96f0557393451c79b6673828ee42c5761e1
Shubhrose Singh <shubhroses@gmail.com>
2023-02-05 18:45:53 -0800

    424

--- medium_questions/424_Longest_repeating_Character_replacement.py
+++ medium_questions/424_Longest_repeating_Character_replacement.py
@@ -22,3 +22,30 @@ class Solution:
             longest_substring_length = end + 1 - start
 
         return longest_substring_length
+
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        """
+        Find longest substring with 2 characters where one element has <= k count
+
+        Find longest substring that has a character with count len - count = k
+
+        maintain a counter 
+        find maximum window that satisfies condition
+
+        len(window) - max(counter.values()) <= k
+        """
+        l = 0
+        res = 0
+        counter = {}
+
+        for r, c in enumerate(s):
+            counter[c] = counter.get(c, 0) + 1
+            while r-l+1 - max(counter.values()) > k:
+                counter[s[l]] -= 1
+                if counter[s[l]] == 0:
+                    del counter[s[l]]
+                l+=1
+            res = max(res, r-l+1)
+        return res
