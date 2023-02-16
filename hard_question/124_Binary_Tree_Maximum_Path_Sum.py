99dc9c3d05b21b331cf45043fb8982f81476506c
Shubhrose Singh <shubhroses@gmail.com>
2023-02-16 15:41:26 -0800

    124

--- hard_question/124_Binary_Tree_Maximum_Path_Sum.py
+++ hard_question/124_Binary_Tree_Maximum_Path_Sum.py
@@ -44,3 +44,36 @@ class Solution:
         
         dfs(root)
         return res
+# Test
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        res = float("-inf")
+
+        def dfs(node):
+            if not node:
+                return 0
+            
+            leftTree = max(dfs(node.left), 0)
+            rightTree = max(dfs(node.right), 0)
+
+            diameter = leftTree + rightTree + node.val
+            nonlocal res
+            res = max(res, diameter)
+
+            return max(leftTree, rightTree) + node.val
+
+
+        dfs(root)
+        return res
+
+"""
+Difficulty: remembering that intitally res should be negative infinity because the result could be a negative number
+remember to ignore paths that have value less than 0
+remember to update res and then return maximum path length only taking nodes in a lines
+"""
