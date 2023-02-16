a2bc67275d8b734f6432c34d351ae1982ddcb522
Shubhrose Singh <shubhroses@gmail.com>
2023-02-15 21:03:59 -0800

    111

--- easy_questions/111_min_depth_of_bst.py
+++ easy_questions/111_min_depth_of_bst.py
@@ -16,3 +16,34 @@ class Solution:
         if res == inf:
             return 0
         return res
+
+# test
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        """
+        Do level order traversal, keep count, if find node withough left or right return count
+        """
+        q = deque([root])
+
+        if not root:
+            return 0
+
+        count = 1
+
+        while q:
+            for _ in range(len(q)):
+                top = q.popleft()
+                if not top.left and not top.right:
+                    return count
+                if top.left:
+                    q.append(top.left)
+                if top.right:
+                    q.append(top.right)
+            count += 1
+        return 0
