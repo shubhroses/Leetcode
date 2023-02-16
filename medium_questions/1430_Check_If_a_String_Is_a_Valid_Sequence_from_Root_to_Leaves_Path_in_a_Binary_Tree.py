28bca7936fe6a682db2f2d205332b433e04116a0
Shubhrose Singh <shubhroses@gmail.com>
2023-02-16 14:49:02 -0800

    1430

---
+++ medium_questions/1430_Check_If_a_String_Is_a_Valid_Sequence_from_Root_to_Leaves_Path_in_a_Binary_Tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidSequence(self, root: Optional[TreeNode], arr: List[int]) -> bool:
+        if not root:
+            return arr == []
+
+        def dfs(node, curPath):
+            if not node.left and not node.right:
+                # print(curPath + [node.val])
+                if arr == curPath + [node.val]:
+                    return True
+            left, right = False, False
+            if node.left:
+                left = dfs(node.left, curPath + [node.val])
+            if node.right:
+                right = dfs(node.right, curPath + [node.val])
+            return left or right
+
+
+        return dfs(root, [])
