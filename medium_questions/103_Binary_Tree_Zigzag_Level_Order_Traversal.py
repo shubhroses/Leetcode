2b1d719e59cc1a34fe14122a193d51f3a1ab85f7
Shubhrose Singh <shubhroses@gmail.com>
2023-02-15 20:56:36 -0800

    103

--- medium_questions/103_Binary_Tree_Zigzag_Level_Order_Traversal.py
+++ medium_questions/103_Binary_Tree_Zigzag_Level_Order_Traversal.py
@@ -25,3 +25,36 @@ class Solution:
             res.append(cur[::flag])
             flag *= (-1)
         return res
+
+# test
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+        if not root:
+            return res
+
+        flag = True
+        
+        q = deque([root])
+        while q:
+            cur = []
+            for _ in range(len(q)):
+                next = q.popleft()
+                cur.append(next.val)
+                if next.left:
+                    q.append(next.left)
+                if next.right:
+                    q.append(next.right)
+            if flag:
+                res.append(cur)
+                flag = False
+            else:
+                res.append(cur[::-1])
+                flag = True
+        return res
