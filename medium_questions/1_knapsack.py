20447760d1579f191789be29fb300d83f60c72a3
Shubhrose Singh <shubhroses@gmail.com>
2023-03-05 20:36:04 -0800

    Knapsack

--- medium_questions/1_knapsack.py
+++ medium_questions/1_knapsack.py
@@ -1,14 +1,23 @@
 def solve_knapsack(profits, weights, capacity):
+    dp = [[-1 for x in range(capacity+1)] for y in range(len(profits))]
     def knapsack_recursive(cap, current_index):
         if cap <= 0 or current_index >= len(profits):
             return 0
+        
+        if dp[current_index][cap] != -1:
+            return dp[current_index][cap]
 
         profit1 = 0
+
+        # Take
         if weights[current_index] <= cap:
             profit1 = profits[current_index] + knapsack_recursive(cap - weights[current_index], current_index + 1)
         
+        # Leave
         profit2 = knapsack_recursive(cap, current_index + 1)
-        return max(profit1, profit2)
+
+        dp[current_index][cap] = max(profit1, profit2)
+        return dp[current_index][cap] 
 
     return knapsack_recursive(capacity, 0)
 
