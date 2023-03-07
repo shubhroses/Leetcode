b058c932153665777cf4d8ce542e7681c6829ccd
Shubhrose Singh <shubhroses@gmail.com>
2023-03-06 21:07:04 -0800

    knapsack bottom up

--- medium_questions/1_knapsack.py
+++ medium_questions/1_knapsack.py
@@ -21,6 +21,32 @@ def solve_knapsack(profits, weights, capacity):
 
     return knapsack_recursive(capacity, 0)
 
+def solve_knapsack_bottom_up(profits, weights, capacity):
+    n = len(profits)
+    if capacity <= 0 or n == 0 or len(weights) != n:
+        return 0
+    
+    dp = [[0 for x in range(capacity + 1)] for y in range(n)]
+
+    for i in range(0, n):
+        dp[i][0] = 0
+    
+    for c in range(0, capacity+1):
+        if weights[0] <= c:
+            dp[0][c] = profits[0]
+    for i in range(1, n):
+        for c in range(1, capacity+1):
+            profit1, profit2 = 0, 0
+
+            if weights[i] <= c:
+                profit1 = profits[i] + dp[i-1][c-weights[i]]
+            
+            profit2 = dp[i-1][c]
+
+            dp[i][c] = max(profit1, profit2)
+    return dp[n-1][capacity]
+
 if __name__ == "__main__":
-    print(solve_knapsack([1, 6, 10, 16], [1, 2, 3, 5], 7))
-    print(solve_knapsack([1, 6, 10, 16], [1, 2, 3, 5], 6))
+    print(solve_knapsack_bottom_up([1, 6, 10, 16], [1, 2, 3, 5], 5))
+    print(solve_knapsack_bottom_up([1, 6, 10, 16], [1, 2, 3, 5], 6))
+    print(solve_knapsack_bottom_up([1, 6, 10, 16], [1, 2, 3, 5], 7))
