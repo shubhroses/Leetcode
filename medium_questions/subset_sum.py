9cf297e01e682b5e1273a387356e009a35d00b73
Shubhrose Singh <shubhroses@gmail.com>
2023-03-11 20:28:20 -0800

    subset sum

--- medium_questions/subset_sum.py
+++ medium_questions/subset_sum.py
@@ -1,19 +1,24 @@
 nums = [3, 34, 4, 12, 5, 2]
 k = 9
 
-def isSubsetSum(i, curSum):
-    if curSum == k:
-        return True
-    if i >= len(nums):
-        return False
-    
-    # Take
-    take = isSubsetSum(i+1, curSum + nums[i])
+dp = [[False for x in range(k+1)] for y in range(len(nums))]
 
-    # Leave
-    leave = isSubsetSum(i+1, curSum)
+def isSubsetSum(i, sum):
+    n = len(nums)
 
-    return take or leave
+    for i in range(0, n):
+        dp[i][0] = True
+
+    for s in range(1, sum+1):
+        dp[0][s] = True if nums[0] == s else False
+
+    for i in range(1, n):
+        for s in range(1, sum+1):
+            if dp[i-1][s]:
+                dp[i][s] = dp[i-1][s]
+            elif s >= nums[i]:
+                dp[i][s] = dp[i-1][s-nums[i]]
+    return dp[n-1][sum]
     
  
  
