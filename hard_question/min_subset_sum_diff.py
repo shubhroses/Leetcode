b07d048453e115ce595b462447c7118f8bd761e2
Shubhrose Singh <shubhroses@gmail.com>
2023-03-14 22:10:45 -0700

    resave

--- hard_question/min_subset_sum_diff.py
+++ hard_question/min_subset_sum_diff.py
@@ -16,7 +16,35 @@ def minimumDifference(nums):
 
     return helper(0, 0, 0)
 
+def minimumDifferenceDP(nums):
+    s = sum(nums)
+    n = len(nums)
+    dp = [[False for x in range(int(s/2)+1)] for y in range(n)]
+
+    for i in range(0, n):
+        dp[i][0] = True
+
+    for j in range(0, int(s/2)+1):
+        dp[0][j] = nums[0] == j
+
+    for i in range(1, n):
+        for j in range(1, int(s/2) + 1):
+            if dp[i-1][j]:
+                dp[i][j] = dp[i-1][j]
+            elif j >= nums[i]:
+                dp[i][j] = dp[i-1][j-nums[i]]
+            
+    sum1 = 0
+
+    for i in range(int(s/2), -1, -1):
+        if dp[n-1][i]:
+            sum1 = i
+            break
+    sum2 = s - sum1
+    return abs(sum2-sum1)
+
+
 if __name__ == "__main__":
-    print(minimumDifference([1, 2, 3, 9]))
-    print(minimumDifference([1, 2, 7, 1, 5]))
-    print(minimumDifference([1, 3, 100, 4]))
+    print(minimumDifferenceDP([1, 2, 3, 9]))
+    print(minimumDifferenceDP([1, 2, 7, 1, 5]))
+    print(minimumDifferenceDP([1, 3, 100, 4]))
