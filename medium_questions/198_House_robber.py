a2606a66d6909fa94e955462d48d2ed62de12aa5
Shubhrose Singh <shubhroses@gmail.com>
2023-06-13 14:19:43 -0500

    resave

--- medium_questions/198_House_robber.py
+++ medium_questions/198_House_robber.py
@@ -21,3 +21,12 @@ class Solution:
             # Skip
 
         return helper(0, False)
+    
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        dp = [0 for _ in range(len(nums) + 1)]
+        dp[1] = nums[0]
+        for i in range(2, len(nums) + 1):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i-1])
+        return dp[-1]
