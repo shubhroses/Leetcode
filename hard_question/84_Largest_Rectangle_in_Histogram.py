ebf0fd0222dbe349ff05df0551701966c68c41ea
Shubhrose Singh <shubhroses@gmail.com>
2023-07-05 20:26:20 -0500

    resave

--- hard_question/84_Largest_Rectangle_in_Histogram.py
+++ hard_question/84_Largest_Rectangle_in_Histogram.py
@@ -89,3 +89,19 @@ class Solution:
             current_width = len(heights) - stack[-1] - 1
             max_area = max(max_area, current_height * current_width)
         return max_area
+    
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        maxArea = 0
+        stack = []  # pair: (index, height)
+
+        for i, h in enumerate(heights):
+            start = i
+            while stack and stack[-1][1] > h:
+                index, height = stack.pop()
+                maxArea = max(maxArea, height * (i - index))
+                start = index
+            stack.append((start, h))
+
+        for i, h in stack:
+            maxArea = max(maxArea, h * (len(heights) - i))
+        return maxArea
