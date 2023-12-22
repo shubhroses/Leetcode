40d4ce638350bb975a094586cad0ef8b0277527a
Shubhrose Singh <shubhroses@gmail.com>
2023-12-22 09:26:02 -0800

    log(n+m)

--- medium_questions/74_search_2d_matrix.py
+++ medium_questions/74_search_2d_matrix.py
@@ -51,3 +51,52 @@ class Solution:
             else:
                 return True
         return False
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        """
+        1. Find the row that the target is in 
+            Find the first row where starting element larger than target then search in row - 1 
+        2. Do binary search in that row
+
+        target = 11
+                       m
+        matrix = [[1],[10],[23]]
+                   t          b
+         
+        candidateRow = -1
+        """
+        t, b = 0, len(matrix)-1
+
+        candidateRow = 0
+
+        while t <= b:
+            m = (t + b)//2
+            if t == b or matrix[m][0] == target:
+                candidateRow = m
+                break
+            elif matrix[m][0] < target:
+                if matrix[m+1][0] > target:
+                    candidateRow = m
+                    break
+                t = m + 1
+    
+            elif matrix[m][0] > target:
+                if m > 0 and matrix[m-1][0] < target:
+                    candidateRow = m-1
+                    break
+                b = m - 1
+
+            
+        print(candidateRow)
+        l, r = 0, len(matrix[0])-1
+        while l <= r:
+            m = (l+r)//2
+            if matrix[candidateRow][m] < target:
+                l = m + 1
+            elif matrix[candidateRow][m] > target:
+                r = m - 1
+            else:
+                return True
+        return False
