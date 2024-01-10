60d604ee441b6ee2df29f6e4d310ae79a75b4544
Shubhrose Singh <shubhroses@gmail.com>
2024-01-10 17:19:01 -0600

    230

--- medium_questions/230_Kth_Smallest_Element_in_a_BST.py
+++ medium_questions/230_Kth_Smallest_Element_in_a_BST.py
@@ -36,3 +36,31 @@ class Solution:
         self.res = None
         helper(root)
         return self.res   
+    
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        """
+        Do In order traversal 
+        Maintain count
+
+        """
+        self.res = -1
+        self.count = 1
+        def helper(node):
+            if not node:
+                return
+            helper(node.left)
+            if self.count == k:
+                self.res = node.val
+            self.count += 1
+            helper(node.right)
+            
+        helper(root)
+        return self.res
