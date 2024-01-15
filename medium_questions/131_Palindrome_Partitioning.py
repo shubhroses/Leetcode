d6bd1182096795beac04ca89450e5a8f3996fa89
Shubhrose Singh <shubhroses@gmail.com>
2024-01-15 17:05:48 -0600

    131

--- medium_questions/131_Palindrome_Partitioning.py
+++ medium_questions/131_Palindrome_Partitioning.py
@@ -31,3 +31,53 @@ class Solution:
                     part.pop()
         helper(0)
         return res
+    
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        """
+        For each i have the option to append it to previous element in cur if it makes a palendrome or add it to a new element. Only have option to add to new element if the previous is a palendrome
+
+        s = "aab"
+        self.res = []
+
+        i = 0
+        cur = []
+            i = 1
+            cur = ["a"]
+                i = 2
+                cur = ["aa"]
+                    i = 3
+                    cur = ["aab]
+
+                    i = 3
+                    cur = ["aa", "b"]
+
+                i = 2
+                cur = ["a", "a"]
+                    i = 3
+                    cur = ["a", "ab"]
+
+                    i = 3
+                    cur = ["a", "a", "b"]
+        """
+
+        self.res = []
+        def helper(i, cur):
+            if i == len(s):
+                if cur[-1] == cur[-1][::-1]:
+                    self.res.append(cur)
+                return
+            if not cur:
+                helper(i+1, [s[i]])
+            else:
+                last = cur[-1]
+                # If last element palendrome can add to prev, or to new
+                helper(i+1, cur[:-1] + [cur[-1]+s[i]])
+                if last == last[::-1]:
+                    helper(i+1, cur + [s[i]])
+                # If last element not palendrom can only add to prev
+        
+        helper(0, [])
+
+        return self.res
