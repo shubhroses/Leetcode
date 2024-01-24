3a49da8cf2f96e605488e0298f876784469bc6c8
Shubhrose Singh <shubhroses@gmail.com>
2024-01-23 18:53:47 -0600

    133 dfs

--- medium_questions/133_Clone_Graph.py
+++ medium_questions/133_Clone_Graph.py
@@ -20,3 +20,32 @@ class Solution:
         return nodeCopy
 
         # dfs iteratively
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return
+        nodeCopy = Node(node.val, [])
+        dic = {node: nodeCopy} # old to new
+
+        def dfs(node):
+            if not node:
+                return
+            for neighbor in node.neighbors:
+                if neighbor not in dic:
+                    neighborCopy = Node(neighbor.val, [])
+                    dic[neighbor] = neighborCopy
+                    dic[node].neighbors.append(neighborCopy)
+                    dfs(neighbor)
+                else:
+                    dic[node].neighbors.append(dic[neighbor])
+        dfs(node)
+        return nodeCopy
