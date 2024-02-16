112b9cbc951a76ba6ed79541837cff5cb9a0e3f0
shubhroses <46800049+shubhroses@users.noreply.github.com>
2024-02-15 18:13:02 -0600

    Update 70_Climbing_Stairs.py

--- easy_questions/70_Climbing_Stairs.py
+++ easy_questions/70_Climbing_Stairs.py
@@ -21,3 +21,29 @@ class Solution:
             x = y
             y = cur
         return cur
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+        memo[1] = 1
+        memo[2] = 2
+        def dp(i):
+            if i in memo:
+                return memo[i]
+            memo[i] = dp(i-1) + dp(i-2)
+            return memo[i]
+        return dp(n)
+
+
+        #edge cases
+        if n==0: return 0
+        if n==1: return 1
+        if n==2: return 2
+        dp = [0]*(n+1) # considering zero steps we need n+1 places
+        dp[1]= 1
+        dp[2] = 2
+        for i in range(3,n+1):
+            dp[i] = dp[i-1] +dp[i-2]
+        print(dp)
+        return dp[n]
+        
