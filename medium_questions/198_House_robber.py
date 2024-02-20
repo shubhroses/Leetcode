2cae4a5d5256b09065a4ef1da56b2cacbb93baab
shubhroses <46800049+shubhroses@users.noreply.github.com>
2024-02-20 16:52:27 -0600

    Update 198_House_robber.py

--- medium_questions/198_House_robber.py
+++ medium_questions/198_House_robber.py
@@ -30,3 +30,37 @@ class Solution:
         for i in range(2, len(nums) + 1):
             dp[i] = max(dp[i-1], dp[i-2] + nums[i-1])
         return dp[-1]
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        """
+        Have a helper function with variables i and can rob
+
+        base case if i == len(nums):
+            return 0
+        
+        Two scenario:
+            if can rob:
+                rob:
+                dont rob
+            if cant rob:
+                dont rob
+        return max of all options
+        """
+        memo = {}
+        def helper(i, canRob):
+            if (i, canRob) in memo:
+                return memo[(i, canRob)]
+            if i == len(nums):
+                return 0
+            
+            rob = 0
+            dontRob = 0
+            if canRob:
+                rob = nums[i] + helper(i+1, False)
+            dontRob = helper(i+1, True)
+
+            memo[(i, canRob)] = max(rob, dontRob)
+            return memo[(i, canRob)]
+        
+        return helper(0, True)
