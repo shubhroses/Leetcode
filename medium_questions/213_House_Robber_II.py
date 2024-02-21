64ccea461941ca96a9c8b500eacc590236b5c68a
shubhroses <46800049+shubhroses@users.noreply.github.com>
2024-02-21 12:19:36 -0600

    Update 213_House_Robber_II.py

--- medium_questions/213_House_Robber_II.py
+++ medium_questions/213_House_Robber_II.py
@@ -67,5 +67,20 @@ class Solution:
             helper(i=0, canRob=True, dontRobLast=True),
             helper(i=1, canRob=True, dontRobLast=False)
         )
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1]))
+
+    def helper(self, nums):
+        rob1, rob2 = 0, 0
+
+        for n in nums:
+            newRob = max(rob1 + n, rob2)
+            rob1 = rob2
+            rob2 = newRob
+        return rob2
+        
+
+
         
 
