f1f578c8440ea57b2e8895c1a00b2b7aaf3e83d1
shubhroses <46800049+shubhroses@users.noreply.github.com>
2024-02-21 12:16:44 -0600

    Update 213_House_Robber_II.py

--- medium_questions/213_House_Robber_II.py
+++ medium_questions/213_House_Robber_II.py
@@ -34,3 +34,38 @@ class Solution:
             return dp[(i, canRob, tuple(arr))]
 
         return max(helper(0, True, nums[1:]), helper(0, True, nums[:-1]))
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        """
+        Do house robbers normally on nums[1:] and nums[:-1]
+
+        Then take the max
+
+
+        """
+        if len(nums) == 1:
+            return nums[0]
+        memo = {}
+        def helper(i, canRob, dontRobLast):
+            if (i, canRob, dontRobLast) in memo:
+                return memo[(i, canRob, dontRobLast)]
+            if dontRobLast:
+                if i == len(nums)-1:
+                    return 0
+            elif i == len(nums):
+                return 0
+            
+            rob = 0
+            if canRob:
+                rob = nums[i] + helper(i+1, False, dontRobLast)
+            leave = helper(i+1, True, dontRobLast)
+
+            memo[(i, canRob, dontRobLast)] = max(rob, leave)
+            return memo[(i, canRob, dontRobLast)]
+        
+        return max(
+            helper(i=0, canRob=True, dontRobLast=True),
+            helper(i=1, canRob=True, dontRobLast=False)
+        )
+        
+
