072c95b19d0167a193211673494adc2cc33c62c6
Shubhrose Singh <shubhroses@gmail.com>
2024-02-21 13:29:49 -0600

    Add solution for decoding ways problem

--- medium_questions/91_decode_ways.py
+++ medium_questions/91_decode_ways.py
@@ -27,3 +27,31 @@ class Solution:
             return memo[i]
 
         return dfs(0)
+    
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        """
+        Need to find number of ways to decode a string
+
+        If at last digit and digit is 0 return 0 else 1
+        If digit starts with 1 or 2 have option to take 1 or 2 digits
+        """
+        # Dynamic Programming
+        dp = {len(s): 1}
+
+        for i in range(len(s)-1, -1, -1):
+            if s[i] == "0":
+                dp[i] = 0
+            else:
+                dp[i] = dp[i+1]
+            if i < len(s) - 1:
+                if s[i] == "1":
+                    dp[i] += dp[i+2]
+                elif s[i] == "2" and s[i+1] in "0123456":
+                    dp[i] += dp[i+2]
+        return dp[0]
+        """
+        Just add whats to the right, and if satisfies situation of a two digits add what two to the right
+        """
+
