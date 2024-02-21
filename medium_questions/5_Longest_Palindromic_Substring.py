93a4fb6fc91c3fdc5b21d7a4c0f33b9cb6eb5f7a
shubhroses <46800049+shubhroses@users.noreply.github.com>
2024-02-21 12:44:15 -0600

    Update 5_Longest_Palindromic_Substring.py

--- medium_questions/5_Longest_Palindromic_Substring.py
+++ medium_questions/5_Longest_Palindromic_Substring.py
@@ -14,3 +14,40 @@ class Solution:
                 start=i-maxLen
                 maxLen+=1
         return s[start:start+maxLen]
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        """
+        1. Starting at every point assume it is the center of a palendromic string, 
+            go out right and left until either side hits start or end of string.
+            or characters are not the same
+            return 
+        2. Starting at every pair, do the same process
+
+        """
+
+        def helper(l, r, cur):
+            # Return pal substr
+            if l < 0 or r >= len(s):
+                return cur
+            
+            if s[l] == s[r]:
+                if l == r:
+                    return helper(l-1, r+1, s[l])
+                return helper(l-1, r+1, s[l] + cur + s[l])
+            
+            return cur
+        
+        res = ""
+        for i in range(len(s)):
+            oddLen = helper(i, i, "")
+            evenLen = ""
+            if i != len(s)-1:
+                evenLen = helper(i, i+1, "")
+
+            if len(oddLen) > len(res):
+                res = oddLen
+            if len(evenLen) > len(res):
+                res = evenLen
+
+        return res
