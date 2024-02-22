4058d37b09abe34f4a983191d48f3c5f0a1bcd56
shubhroses <46800049+shubhroses@users.noreply.github.com>
2024-02-22 15:39:30 -0600

    Update 322_Coin_Change.py

--- medium_questions/322_Coin_Change.py
+++ medium_questions/322_Coin_Change.py
@@ -34,3 +34,42 @@ class Solution:
             for i in range(coin, amount+1):
                 dp[i] = min(dp[i], dp[i-coin]+1)
         return dp[amount] if dp[amount] != math.inf else -1
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        """
+        Write a helper function that returns the min num of coins to get to amount
+        And inf if not possible
+        """
+
+        dp = [float("inf") for _ in range(amount+1)]
+        dp[0] = 0
+        for coin in coins:
+            for i in range(coin, amount+1):
+                dp[i] = min(dp[i], dp[i-coin] + 1)
+        return dp[amount] if dp[amount] != float("inf") else -1
+
+
+
+
+        return 0
+
+        memo = {}
+        def helper(curAmount):
+            if curAmount in memo:
+                return memo[curAmount]
+            if curAmount > amount:
+                return float("inf")
+            if curAmount == amount:
+                return 0
+            
+            res = float("inf")
+            for coin in coins:
+                res = min(res, 1 + helper(curAmount + coin))
+            memo[curAmount] = res
+            return memo[curAmount]
+        
+        result = helper(0)
+        if result == float("inf"):
+            return -1
+        return result
