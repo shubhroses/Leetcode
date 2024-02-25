b78f4b3bd310f02a5a203607fcb244e11852c2e4
shubhroses <46800049+shubhroses@users.noreply.github.com>
2024-02-25 17:12:41 -0600

    Update 139_word_break.py

--- medium_questions/139_word_break.py
+++ medium_questions/139_word_break.py
@@ -26,3 +26,28 @@ class Solution:
             memo[(l, r)] = leave or take
             return memo[(l, r)]
         return inDict(0, 0)
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        """
+        Write helper function with l and r pointers
+        if s[l:r+1] in word can take or leave
+            take: l = r+1, r = r+1
+            leave: l = l, r = r+1
+
+             01234567
+        s = "leetcode", wordDict = ["leet","code"]
+                    l
+                    r
+        """
+        dp = [False] * (len(s) + 1)
+        dp[len(s)] = True
+
+        for i in range(len(s) - 1, -1, -1):
+            for w in wordDict:
+                if (i + len(w)) <= len(s) and s[i : i + len(w)] == w:
+                    dp[i] = dp[i + len(w)]
+                if dp[i]:
+                    break
+
+        return dp[0]
