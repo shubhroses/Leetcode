3c31c36eac9773c332b75fcb3ef3821ff4a13c76
shubhroses <46800049+shubhroses@users.noreply.github.com>
2024-02-25 17:21:48 -0600

    Update 300_Longest_Increasing_Subsequence.py

--- medium_questions/300_Longest_Increasing_Subsequence.py
+++ medium_questions/300_Longest_Increasing_Subsequence.py
@@ -41,3 +41,32 @@ class Solution:
             return helper(i+1, min)
         
         """
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        """
+        Write helper function that returns length of longest increasing subseq
+
+        if i > len(nums): return 0
+
+        if nums[i] > prev: take or leave
+
+        [0,1,0,3,2,3]
+    p.   
+                   i
+
+        """
+        memo = {}
+        def length(prev, i):
+            if (prev, i) in memo:
+                return memo[(prev, i)]
+            if i >= len(nums):
+                return 0
+            take = 0
+            if nums[i] > prev:
+                take = length(nums[i], i + 1) + 1
+            leave = length(prev, i + 1)
+            memo[(prev, i)] = max(take, leave)
+            return memo[(prev, i)]
+
+        return length(float("-inf"), 0)
+        
