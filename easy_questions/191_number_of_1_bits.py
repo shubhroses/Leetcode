4541395ea6579d2d3e3f7e4dce1d727933f72327
shubhroses <46800049+shubhroses@users.noreply.github.com>
2024-03-27 14:38:59 -0500

    Update 191_number_of_1_bits.py

--- easy_questions/191_number_of_1_bits.py
+++ easy_questions/191_number_of_1_bits.py
@@ -1,1 +1,13 @@
-res = 0
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        """
+        if num & 1 == 1 then increment
+        shift num down
+        keep going until no n
+        """
+        res = 0
+        while n:
+            if n & 1 == 1:
+                res += 1
+            n  = n >> 1
+        return res
