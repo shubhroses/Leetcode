45ff656c354d46f7e5abe6e24fdec089178274b3
shubhroses <46800049+shubhroses@users.noreply.github.com>
2024-03-28 18:05:14 -0500

    Update 338_count_bits.py

--- easy_questions/338_count_bits.py
+++ easy_questions/338_count_bits.py
@@ -1,1 +1,21 @@
 
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        """
+        Brute force for each number ranging from 0 to n inclusive, 
+        Determine the number of 1 bits, and append to result arr
+
+        """
+        res = []
+
+        def getCount(i):
+            count = 0
+            while i:
+                if i & 1 == 1:
+                    count += 1
+                i = i >> 1
+            return count
+
+        for i in range(n+1):
+            res.append(getCount(i))
+        return res
