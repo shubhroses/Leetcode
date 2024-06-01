dae27732fd2ddb583b06e4399fc79fbd13c007b6
shubhroses <46800049+shubhroses@users.noreply.github.com>
2024-06-01 12:46:37 -0500

    Update 57_Insert_interval.py

--- medium_questions/57_Insert_interval.py
+++ medium_questions/57_Insert_interval.py
@@ -22,3 +22,41 @@ class Solution:
         # Add remaining intervals
         merged += intervals[i:]
         return merged
+
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        """
+        si ei 
+
+        sn en 
+
+        while sn > ei keep current intervals
+
+        if sn between si and ei
+            keep si
+            keep max of en ei
+        
+        if si <= end of last element, update last element with max of cur and new last en
+        
+        [[1, 3], [4, 6]]  [2 , 5]
+                  si ei  
+        
+        res = [[1, 5]]
+        """
+
+        res = []
+
+        for i in range(len(intervals)):
+            if newInterval[1] < intervals[i][0]:
+                res.append(newInterval)
+                return res + intervals[i:]
+            elif newInterval[0] > intervals[i][1]:
+                res.append(intervals[i])
+            else:
+                newInterval = [
+                    min(newInterval[0], intervals[i][0]),
+                    max(newInterval[1], intervals[i][1]),
+                ]
+        res.append(newInterval)
+        return res
