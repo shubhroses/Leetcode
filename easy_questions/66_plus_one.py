128e8fff5b1e2e1a7d983423a3f51970cb99f43c
shubhroses <46800049+shubhroses@users.noreply.github.com>
2024-06-24 19:22:15 -0500

    Update 66_plus_one.py

--- easy_questions/66_plus_one.py
+++ easy_questions/66_plus_one.py
@@ -5,4 +5,33 @@ class Solution:
         res = [int(c) for c in str(num)]
         return res
 
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        """
+        increment the last number, if last number is 9 set to 0
+        if last number is 0, need to increment number ot left
+
+
+        9
+
+        0
+        i
+        prevZero = True
+        """
+        reversed = digits[::-1]
+        prevZero = True
+        i = 0
+        while i < len(digits) and prevZero:
+            reversed[i] = (reversed[i] + 1) % 10
+            if reversed[i] != 0:
+                prevZero = False
+            i += 1
+        if prevZero:
+            reversed.append(1)
+        return reversed[::-1]
+
+
+        
+
         
