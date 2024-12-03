db889c8d52bc220980a7751725aea221a2913553
shubhroses <46800049+shubhroses@users.noreply.github.com>
2024-12-03 11:48:21 -0600

    Update 1757_Recyclable_and_Low_Fat_Products.sql

--- easy_questions/1757_Recyclable_and_Low_Fat_Products.sql
+++ easy_questions/1757_Recyclable_and_Low_Fat_Products.sql
@@ -1,3 +1,10 @@
 # Write your MySQL query statement below
 Select product_id from Products p
 where p.low_fats = 'Y' and p.recyclable = 'Y'
+
+-- Write your MySQL query statement below
+select product_id
+from Products
+where low_fats = 'Y'
+and recyclable = 'Y';
+
