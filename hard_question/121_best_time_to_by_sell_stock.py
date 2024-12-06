3e9f83f8efc3c984b969998412ae69d25821cffd
shubhroses <46800049+shubhroses@users.noreply.github.com>
2024-12-06 12:47:51 -0600

    Create 121_best_time_to_by_sell_stock.py

---
+++ hard_question/121_best_time_to_by_sell_stock.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        """
+        Iterate through l, maintaining min prices
+
+        Maintian res which is max cur pric - min price 
+
+        [7,1,5,3,6,4]
+         L
+
+         min = inf
+         res = -inf
+        
+        min = min(min, l)
+        res = max(res, l - min)
+
+        """
+        res = float("-inf")
+        curMin = float("inf")
+
+        for ind, cost in enumerate(prices):
+            curMin = min(curMin, cost)
+            res = max(res, cost - curMin)
+
+        return res
