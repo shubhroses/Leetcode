15088958863ffd85f4d703847009cd2189232760
shubhroses <46800049+shubhroses@users.noreply.github.com>
2024-12-09 06:51:50 -0600

    Update 3_Longest_Substring_Without_Repeating_Characters.py

--- medium_questions/3_Longest_Substring_Without_Repeating_Characters.py
+++ medium_questions/3_Longest_Substring_Without_Repeating_Characters.py
@@ -23,3 +23,36 @@ class Solution:
             visited.add(c)
             res = max(res, r-l+1)
         return res
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        """
+        Maintain a left a right pointer
+        a map of all elements and position in window
+        If the right pointer encounters a duplicate elements
+        move the left pointer until reach index of previos elements
+        Maintain a result array
+
+        012345
+        pwwkew
+              l
+             r
+
+        window = {w, k, e}
+        res = 3
+        """
+        l = 0
+        window = set()
+        res = 0
+
+        for r in range(len(s)):
+            while s[r] in window:
+                window.remove(s[l])
+                l += 1
+            window.add(s[r])
+            res = max(res, r-l+1)
+        return res
+
+
+
+
+        
