1181793d70da60360650b451a156262a4905d7f9
shubhroses <46800049+shubhroses@users.noreply.github.com>
2024-12-13 14:23:36 -0600

    Update 143_Reorder_list.py

--- medium_questions/143_Reorder_list.py
+++ medium_questions/143_Reorder_list.py
@@ -50,3 +50,56 @@ class Solution:
         
         
         return head
+        
+        
+        class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        1 -> 2 -> 3 -> 4 -> 5
+                  s 
+                                f
+        """
+        slow = head
+        fast = head.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next
+            if fast:
+                fast = fast.next
+            
+        l1 = head
+        l2 = slow.next
+        slow.next = None
+
+        def reverseList(start):
+            prev = None
+            cur = start
+
+            while cur:
+                next = cur.next
+                cur.next = prev
+                prev = cur
+                cur = next
+            return prev
+        
+        l2 = reverseList(l2)
+
+        dummy = ListNode()
+        cur = dummy
+        while l1 and l2:
+            cur.next = l1
+            l1next = l1.next
+            cur = cur.next
+            l1 = l1next
+
+            cur.next = l2
+            l2next = l2.next
+            cur = cur.next
+            l2 = l2next
+        
+        if l1:
+            cur.next = l1
+
+        return dummy.next
+        
