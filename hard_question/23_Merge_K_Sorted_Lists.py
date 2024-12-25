25b5205604e22e5a3ae4b13f7f028b33b8aaaa1b
shubhroses <46800049+shubhroses@users.noreply.github.com>
2024-12-25 13:08:03 -0600

    Update 23_Merge_K_Sorted_Lists.py

--- hard_question/23_Merge_K_Sorted_Lists.py
+++ hard_question/23_Merge_K_Sorted_Lists.py
@@ -126,4 +126,65 @@ class Solution:
                 lists[i] = lists[i].next
         
         return head.next
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        """
+        Write a helper function that merges 2 sorted lists
+        while len of lists > 2, keep merging 2 and appending to lists
+        return the one list
+
+
+        1 -> 4 -> 5
+
+        1 -> 3
+        """
+        if not lists:
+            return None
+
+        def merge2(l1, l2):
+            dummy = ListNode()
+            cur = dummy
+            while l1 and l2:
+                if l1.val < l2.val:
+                    cur.next = l1
+                    l1 = l1.next
+                else:
+                    cur.next = l2
+                    l2 = l2.next
+                cur = cur.next
+            while l1:
+                cur.next = l1
+                l1 = l1.next
+                cur = cur.next
+            while l2:
+                cur.next = l2
+                l2 = l2.next
+                cur = cur.next
+            return dummy.next
+        
+        while len(lists) >= 2:
+            l1 = lists.pop()
+            l2 = lists.pop()
+            newList = merge2(l1, l2)
+            lists.append(newList)
+
+        return lists[0]
+        """
+        [1,4,5]]
+
+        l1: 3,4
+        l2: 2,6
+
+        d -> 1
+
+             c
+        """
+
         
