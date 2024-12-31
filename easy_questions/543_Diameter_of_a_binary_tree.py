f44f00c997273d0abca7d7146ccda6d3a8fd81d8
shubhroses <46800049+shubhroses@users.noreply.github.com>
2024-12-31 11:57:36 -0600

    Update 543_Diameter_of_a_binary_tree.py

--- easy_questions/543_Diameter_of_a_binary_tree.py
+++ easy_questions/543_Diameter_of_a_binary_tree.py
@@ -22,3 +22,25 @@ class Solution:
 Difficulty: Forgot to call dfs function
  learned to uses nonlocal to use a variable in a function that is first created in parent function
 """
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        stack = [root]
+        mp = {None: (0, 0)}
+
+        while stack:
+            node = stack[-1]
+
+            if node.left and node.left not in mp:
+                stack.append(node.left)
+            elif node.right and node.right not in mp:
+                stack.append(node.right)
+            else:
+                node = stack.pop()
+
+                leftHeight, leftDiameter = mp[node.left]
+                rightHeight, rightDiameter = mp[node.right]
+
+                mp[node] = (1 + max(leftHeight, rightHeight),
+                           max(leftHeight + rightHeight, leftDiameter, rightDiameter))
+
+        return mp[root][1]
