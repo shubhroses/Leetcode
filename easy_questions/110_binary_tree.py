75b35f17c3b6cfa4e8d2e11ac10208b14311ede7
shubhroses <46800049+shubhroses@users.noreply.github.com>
2025-01-01 13:42:09 -0600

    Update 110_binary_tree.py

--- easy_questions/110_binary_tree.py
+++ easy_questions/110_binary_tree.py
@@ -6,3 +6,34 @@ class Solution(object):
         leftheight, rightheight = self.Height(root.left), self.Height(root.right)
         if leftheight < 0 or rightheight < 0 or abs(leftheight - rightheight) > 1:  return -1
         return max(leftheight, rightheight) + 1
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        """
+        Helper function that returns depth
+        if they differ 
+        """
+        self.res = True
+
+        def helper(node):
+            if not node:
+                return 0
+            
+            left = helper(node.left)
+            right = helper(node.right)
+
+            if abs(left - right) > 1:
+                self.res = False
+                return float("inf")
+            
+            return max(left, right) + 1
+
+        helper(root)
+        return self.res
