4d1f53fcc5b50d9fd541cbd475ecfee514722f22
shubhroses <46800049+shubhroses@users.noreply.github.com>
2025-01-08 09:51:21 -0600

    Update 1448_Count_good_Node_in_Binary_tree.py

--- medium_questions/1448_Count_good_Node_in_Binary_tree.py
+++ medium_questions/1448_Count_good_Node_in_Binary_tree.py
@@ -23,3 +23,38 @@ class Solution:
             helper(node.right, newMax)
         helper(root, float("-inf"))
         return res
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+"""
+    2
+      4
+    10  8
+    
+"""
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+
+        self.res = 0
+
+        def helper(node, maxPath):
+            if not node:
+                return
+            
+            if node.val >= maxPath:
+                self.res += 1
+            
+            helper(node.left, max(node.val, maxPath))
+            helper(node.right, max(node.val, maxPath))
+
+        helper(root, root.val)
+
+        return self.res
+
