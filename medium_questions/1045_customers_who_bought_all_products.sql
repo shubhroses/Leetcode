42d522ce99f457a9239401714c1b1bf5577332f9
shubhroses <46800049+shubhroses@users.noreply.github.com>
2025-01-08 09:57:22 -0600

    Create 1045_customers_who_bought_all_products.sql

---
+++ medium_questions/1045_customers_who_bought_all_products.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+with numDistinctProds as (
+    select count(*) as numProds
+    from Product
+),
+countProdsBoughtByCustomer as (
+    select 
+        customer_id,
+        count(distinct product_key) as boughtProds
+    from Customer 
+    group by customer_id
+)
+select c.customer_id
+from countProdsBoughtByCustomer c
+join numDistinctProds p
+where c.boughtProds = p.numProds;
+
