feaab7d4a123763af8b700b3a8f0db449b5feacc
shubhroses <46800049+shubhroses@users.noreply.github.com>
2025-01-15 09:56:28 -0600

    Update 180_Consecutive_numbers.sql

--- medium_questions/180_Consecutive_numbers.sql
+++ medium_questions/180_Consecutive_numbers.sql
@@ -4,3 +4,20 @@ from Logs One
 join Logs Two on One.num = Two.num
 join Logs Three on Three.num = Two.num
 where One.id + 1 = Two.id and Two.id + 1 = Three.id;
+
+
+
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+with cte as (
+    select num,
+    lead(num,1) over() num1,
+    lead(num,2) over() num2
+    from logs
+
+)
+
+select distinct num ConsecutiveNums 
+from cte 
+where (num=num1) and (num=num2)
+
