b136f0d424e698385983aca5950de642c5a213cf
shubhroses <46800049+shubhroses@users.noreply.github.com>
2025-01-16 12:22:22 -0600

    Update 124_Binary_Tree_Maximum_Path_Sum.py

--- hard_question/124_Binary_Tree_Maximum_Path_Sum.py
+++ hard_question/124_Binary_Tree_Maximum_Path_Sum.py
@@ -91,3 +91,32 @@ Difficulty: remembering that intitally res should be negative infinity because t
 remember to ignore paths that have value less than 0
 remember to update res and then return maximum path length only taking nodes in a lines
 """
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        """
+        Create helper function that returns max path withouth branching
+        miantain res that gets update with non branching paths
+        """
+
+        self.res = float("-inf")
+
+        def helper(node):
+            if not node:
+                return 0
+            
+            left = helper(node.left)
+            right = helper(node.right)
+
+            self.res = max(self.res, left + node.val, right + node.val, node.val, left + right + node.val)
+
+            return max(left + node.val, right + node.val, node.val)
+        
+        helper(root)
+ 
+        return self.res
