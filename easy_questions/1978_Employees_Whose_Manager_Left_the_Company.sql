c4761cbbae895f4ef455c3fe656c30bfd9a6f186
shubhroses <46800049+shubhroses@users.noreply.github.com>
2025-01-22 17:24:52 -0600

    Create 1978_Employees_Whose_Manager_Left_the_Company.sql

---
+++ easy_questions/1978_Employees_Whose_Manager_Left_the_Company.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select employee_id
+from Employees 
+where manager_id not in (select distinct employee_id from Employees)
+and salary < 30000
+order by employee_id;
+
