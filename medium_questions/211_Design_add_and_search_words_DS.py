241d66ecb6379a8f0d7f6f1542c1a383d38a8027
shubhroses <46800049+shubhroses@users.noreply.github.com>
2025-01-24 13:12:06 -0600

    Update 211_Design_add_and_search_words_DS.py

--- medium_questions/211_Design_add_and_search_words_DS.py
+++ medium_questions/211_Design_add_and_search_words_DS.py
@@ -27,3 +27,72 @@ class WordDictionary:
             return False
     
         return dfs(self.root, 0)
+
+
+
+"""
+To add a word need a node with dictionary, letter, and isLast
+
+add:
+    for c in Word
+        if c in dictionary, go to next letter and next node
+        if c not in dictionary, add to dictinary and create a new node 
+    set is word to true
+
+search:
+    for c in word:
+        if c is ., let next be any element in dict
+    if is word return
+
+    bad
+    i
+
+    start = ({'b': }, False) 
+    ({a: }, False)
+    ({d}, False)
+    ({}, True) cur
+
+
+"""
+class Node:
+    def __init__(self):
+        self.dict = {}
+        self.isWord = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.start = Node()
+
+    def addWord(self, word: str) -> None:
+        cur = self.start
+        for c in word:
+            if c not in cur.dict:
+                cur.dict[c] = Node()
+            cur = cur.dict[c]
+        cur.isWord = True
+
+    def search(self, word: str) -> bool:
+        def isInDict(cur, i):
+            if i == len(word):
+                return cur.isWord
+            if word[i] != '.':
+                if word[i] not in cur.dict:
+                    return False
+                else:
+                    return isInDict(cur.dict[word[i]], i + 1)
+            else:
+                for k, v in cur.dict.items():
+                    if isInDict(cur.dict[k], i + 1):
+                        return True
+                return False
+        
+        return isInDict(self.start, 0)
+
+        
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
