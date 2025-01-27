0934e969eaeb09750a7df99490ba8a57d2d54ba7
Shubhrose Singh <shubhroses@gmail.com>
2025-01-27 17:02:13 -0600

    Commit

--- medium_questions/981_time_based_key_value_store.py
+++ medium_questions/981_time_based_key_value_store.py
@@ -1,35 +1,46 @@
 class TimeMap:
 
     def __init__(self):
-        self.savedDict = collections.defaultdict(list)
+        self.map = collections.defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        self.savedDict[key].append((timestamp, value))
+        self.map[key].append((value, timestamp))
 
     def get(self, key: str, timestamp: int) -> str:
-        if key in self.savedDict:
-            allVals = self.savedDict[key]
-            if not allVals or timestamp < allVals[0][0]:
-                return ""
+        # If the key does not exist, return an empty string
+        if key not in self.map:
+            return ""
+        
+        arr = self.map[key]
+        l, r = 0, len(arr) - 1
+        result = ""
+        
+        # Perform binary search
+        while l <= r:
+            m = (l + r) // 2
+            if arr[m][1] <= timestamp:
+                # Update result and move to the right to find the largest timestamp
+                result = arr[m][0]
+                l = m + 1
+            else:
+                # Move to the left
+                r = m - 1
 
-            "Find first element with value lower than target"
-            l, r = 0, len(allVals) - 1
-            while l <= r:
-                m = (l + r) // 2
-                if allVals[m][0] <= timestamp:
-                    if m == len(allVals)-1:
-                        return allVals[m][1]
-                    if allVals[m+1][0] > timestamp:
-                        return allVals[m][1]
-                    l = m + 1
-                else:
-                    r = m - 1
-            
-        else:
-            return "" 
-
+        return result
 
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
 # obj.set(key,value,timestamp)
 # param_2 = obj.get(key,timestamp)
+
+"""
+have a map (key, [value, timestamp]) where the list is sorted based on timestamp
+
+on a set append to list
+
+on a get 
+    search list using binary search to find pair 
+    where m[1] <= target and m+1[1] > target
+
+    981_Time_Based_Key_Value_Store.py
+"""
