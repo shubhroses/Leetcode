7f50bfbc8739e7b246187643e7af750ba76b4e88
shubhroses <46800049+shubhroses@users.noreply.github.com>
2025-01-27 16:59:20 -0600

    Create medium_questions1321_Restaurant_Growth.sql

---
+++ medium_questions1321_Restaurant_Growth.sql
@@ -0,0 +1,20 @@
+select visited_on,
+    (
+        SELECT SUM(amount)
+        FROM customer
+        WHERE visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY) AND c.visited_on
+    ) AS amount,
+    ROUND(
+        (
+            SELECT SUM(amount) / 7
+            FROM customer
+            WHERE visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY) AND c.visited_on
+        ),
+        2
+    ) AS average_amount
+from customer c
+where visited_on >= (
+    select date_add(min(visited_on), interval 6 day)
+    from customer
+)
+group by visited_on;
