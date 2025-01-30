96bb63adc28830ff2f9570868fa6a743ad0e3407
shubhroses <46800049+shubhroses@users.noreply.github.com>
2025-01-29 18:30:37 -0600

    Update 78_subsets.py

--- medium_questions/78_subsets.py
+++ medium_questions/78_subsets.py
@@ -37,3 +37,25 @@ for each element in res, add a duplicate with an appended new element
 
         dfs(0)
         return res
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        subset = []
+
+        def helper(i):
+            if i == len(nums):
+                res.append(subset.copy())
+                return
+            
+            # Take
+            subset.append(nums[i])
+            helper(i+1)
+
+            # Leave
+            subset.pop()
+            helper(i+1)
+        
+        helper(0)
+
+        return res
