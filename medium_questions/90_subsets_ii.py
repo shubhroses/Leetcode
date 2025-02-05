8cfc283baf374bdc09432b4b1db19ff3fd30bad8
shubhroses <46800049+shubhroses@users.noreply.github.com>
2025-02-05 17:01:28 -0600

    Update 90_subsets_ii.py

--- medium_questions/90_subsets_ii.py
+++ medium_questions/90_subsets_ii.py
@@ -39,3 +39,26 @@ class Solution:
         helper(0, [])
 
         return self.res
+
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        """
+        A teach index can take or leave
+        """
+        res = set()
+
+        def backtrack(i, subset):
+            if i == len(nums):
+                res.add(tuple(subset))
+                return
+
+            subset.append(nums[i])
+            backtrack(i + 1, subset)
+            subset.pop()
+            backtrack(i + 1, subset)
+
+        nums.sort()
+        backtrack(0, [])
+        return [list(s) for s in res]
+
