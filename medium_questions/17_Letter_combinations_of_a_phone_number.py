871fe13ff7a13098d9676e82f56c2e3190361778
shubhroses <46800049+shubhroses@users.noreply.github.com>
2025-02-11 16:46:54 -0600

    Update 17_Letter_combinations_of_a_phone_number.py

--- medium_questions/17_Letter_combinations_of_a_phone_number.py
+++ medium_questions/17_Letter_combinations_of_a_phone_number.py
@@ -33,3 +33,37 @@ class Solution:
             
         helper(0, "")
         return res
+        
+        
+        
+        
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        numToChars = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"]
+        }
+
+        res = []
+
+        def helper(i, cur):
+            if i == len(digits):
+                if cur:
+                    res.append(cur)
+                return
+            
+            for char in numToChars[digits[i]]:
+                newCur = cur + char
+                helper(i+1, newCur)
+        
+        helper(0, "")
+
+        return res
+
+
