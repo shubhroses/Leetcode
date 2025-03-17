df5bf22bb45c0f9588c06f1c806e39a95233712e
shubhroses <46800049+shubhroses@users.noreply.github.com>
2025-03-17 16:15:35 -0500

    Update 200_number_of_islands.py

--- medium_questions/200_number_of_islands.py
+++ medium_questions/200_number_of_islands.py
@@ -18,3 +18,25 @@ class Solution:
                     dfs(r,c)
                     res += 1
         return res
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def helper(r, c):
+            if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] == '0':
+                return
+            
+            grid[r][c] = '0'
+
+            helper(r+1, c)
+            helper(r-1, c)
+            helper(r, c+1)
+            helper(r, c-1)
+
+        res = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == '1':
+                    helper(r, c)
+                    res += 1
+        return res
