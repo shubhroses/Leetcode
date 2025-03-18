12bdf5a3dd17073aaf23738dbae05dab6356cfb2
shubhroses <46800049+shubhroses@users.noreply.github.com>
2025-03-18 11:00:22 -0500

    Update 695_max_area_of_island.py

--- medium_questions/695_max_area_of_island.py
+++ medium_questions/695_max_area_of_island.py
@@ -21,3 +21,33 @@ class Solution:
                 self.res = max(self.res, area)
 
         return self.res
+        
+        
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        """
+        Do bfs with queue
+        For every 1 increment count
+        """
+
+        def helper(r,c):
+            if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] == 0:
+                return 0
+            grid[r][c] = 0
+            
+            right = helper(r+1, c)
+            left = helper(r-1, c)
+            down = helper(r, c+1)
+            up = helper(r, c-1)
+            
+            return right + left + down + up + 1
+            
+
+
+        res = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 1:
+                    res = max(helper(r, c), res)
+        return res
+        
