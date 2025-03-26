a4fb04255bc8fa7cad3ff88c18814a76bff3c127
shubhroses <46800049+shubhroses@users.noreply.github.com>
2025-03-26 13:38:47 -0500

    Update 329_Longest_Increasing_Path_In_Matrix.py

--- hard_question/329_Longest_Increasing_Path_In_Matrix.py
+++ hard_question/329_Longest_Increasing_Path_In_Matrix.py
@@ -28,3 +28,58 @@ class Solution:
                 res = max(res, dfs(i, j, float("-inf")))
                 
         return res
+
+
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        """
+        Starting at each node, we can do a dfs saving the length and dont visit visited nodes
+
+
+        dfs:
+            if out of bounds return 0
+            if cur val <= prev val:
+                return 0
+            
+            if r, c in visitedL
+                return 0
+            
+            go in all 4 directions
+            return max of those 4 directions
+
+            remove from visited
+
+
+        
+        r, c = 0, 0
+        res = 0
+        visited = {(0, 0)}
+        rev = -inf
+
+        [[1,2]]
+
+
+        if you are at a cell that has already been visited as a part of a longer path
+        """
+        res = 1
+        self.visited = {}
+
+        def dfs(r, c, prev):
+            if r < 0 or r >= len(matrix) or c < 0 or c >= len(matrix[0]) or matrix[r][c] <= prev:
+                return 0
+            if (r, c) in self.visited:
+                return self.visited[(r, c)]
+            left = dfs(r-1, c, matrix[r][c])
+            right = dfs(r+1, c, matrix[r][c])
+            down = dfs(r, c+1, matrix[r][c])
+            up = dfs(r, c-1, matrix[r][c])
+
+            res = max(1, max(left, right, down, up) + 1)
+            self.visited[(r, c)] = res
+            return self.visited[(r, c)]
+        
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+                res = max(res, dfs(r, c, float("-inf")))
+
+        return res
