8b3ce18c937cda70755e54fcd34d8481bfde3b77
shubhroses <46800049+shubhroses@users.noreply.github.com>
2025-06-18 16:32:42 +0000

    Add trailing # (cycle 1 on 2025-01-01)

--- easy_questions/1_Two_Sum.py
+++ easy_questions/1_Two_Sum.py
@@ -4,4 +4,4 @@ class Solution:
         for i, e in enumerate(nums):
             if target-e in elements:
                 return [i, elements[target-e]]
-            elements[e] = i
+            elements[e] = i#
